baf19d48be31d2be2827a291a4626ea6a6a44747
AhmedFayed123 <medofayed123@gmail.com>
2023-01-02 00:39:56 +0200

    first commit

---
+++ Source.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+using namespace std;
+int main() {
+	int x, y, z, sum;
+	cout << "enter three digit"<<"\n";
+	cin >> x >> y >> z;
+	sum = x + y + z;
+	cout << "sum is:" <<sum<< endl;
+	return 0;
+}
